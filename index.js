2a3a2a9800c4f5464fccd517595236e72fb5d413
Hans Wong <wonghans@hotmail.com>
2022-08-05 10:45:59 +0800

    Code for signal server completed

--- index.js
+++ index.js
@@ -21,17 +21,38 @@ io.on("connection", (socket) =>{
         if (room == undefined){
             // create a room if undefined
             socket.join(roomName)
-            console.log ("Room Created")
+            socket.emit("created")
         }
         else if (room.size==1){
             socket.join(roomName)
-            console.log("Room Joined")
+            socket.emit("joined")
+
         }
         else{
-            console.log("Room Full for Now") 
+            socket.emit("full")
         }
         console.log(rooms)
 
+    })
+
+    socket.on("ready", (roomName) => {
+        console.log("Ready")
+        socket.broadcast.to(roomName).emit("ready")
+    })
+
+    socket.on("candidate", (candidate, roomName) => {
+        console.log("Candidate")
+        socket.broadcast.to(roomName).emit("candidate", candidate)
+    })
+
+    socket.on("offer", (offer, roomName) => {
+        console.log("Offer")
+        socket.broadcast.to(roomName).emit("offer", offer)
+    })
+
+    socket.on("answer", (answer, roomName) => {
+        console.log("Answer")
+        socket.broadcast.to(roomName).emit("answer", answer)
     })
 })
 
